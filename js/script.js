63dbed1d7d86e2c98f30b9be883ef484575de64a
Churchinruins <surhoisaev@gmail.com>
2024-10-24 18:09:26 +0300

    change:function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,41 @@
-let num = 266219
-let number = num.toString();
-console.log(number.split('').reduce((acc, item) => acc * item, 1))
-const result1 = number.split('').reduce((acc, item) => acc * item, 1)
-const result2 = result1 ** 3
-console.log(result2)
-const result3 = result2
-console.log(result2.toString().slice(0, 2))
+let lang = 'en';
+let weekDays;
+if (lang === 'ru') {
+    weekDays = ('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
+} else if (lang === 'en') {
+    weekDays = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+}
+
+let langDay = 'ru';
+let days;
+switch (langDay) {
+    case 'ru':
+        days = ('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
+        break;
+    case 'en':
+        days = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+        break;
+}
+
+let namePerson = "Артем";
+let role = namePerson === "Артем" ? "директор" :
+    namePerson === "Александр" ? "преподаватель" :
+        "студент";
+
+console.log(weekDays);
+console.log(days);
+console.log(role);
+
+
+function hardWorkLesson(text) {
+    if (typeof text !== 'string') {
+        return "Тут должна быть строка, СТРОГО";
+    }
+    const stringWithoutSpace = text.trim();
+    if (stringWithoutSpace.length > 30) {
+        return stringWithoutSpace.slice(0, 30) + "...";
+    }
+    return stringWithoutSpace;
+}
+
+console.log(hardWorkLesson(12344565))
